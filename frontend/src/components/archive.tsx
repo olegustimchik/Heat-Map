bd7fc7200bfcc53189309082d0f820c88ffaf858
olegustimchik <71694504+olegustimchik@users.noreply.github.com>
2024-04-04 20:30:04 +0300

    finish work with front

---
+++ frontend/src/components/archive.tsx
@@ -0,0 +1,9 @@
+export function Archive() {
+  return (
+    <section className="archive-section">
+      <a href="http://localhost:3000/archive">
+        <button className="archive-button">Download Archive</button>
+      </a>
+    </section>
+  );
+}
